1597b1045aea984297c7702a9d29458833a2d138
Simas Jakubenas <sjakubenas@gmail.com>
2023-11-05 12:30:13 +0000

    Add character classes

--- run.py
+++ run.py
@@ -132,4 +132,33 @@ def sewers_zone_navigation():
         if sewers_controls == '4' and y > 0:
             y -= 1
 
+# Classes
+class Character():
+    """
+    Superclass for all playeable characters and enemies in the game
+    """
+    # properties
+    def __init__(self, *stats):
+        self.name = stats[0]
+        self.health = stats[1]
+        self.max_health = stats[1]
+        self.attack = stats[2]
+        self.gold = stats[3]
+
+class Hero(Character):
+    """
+    Playable characters subclass
+    """
+    def __init__(self, *stats):
+        super().__init__(*stats)
+        self.health_pot = stats[4]
+
+class Enemy(Character):
+    """
+    Encounterable enemies subclass
+    """
+    def __init__(self, *stats):
+        super().__init__(*stats)
+        self.spawn_area = stats[4]
+
 main()
