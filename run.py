5726c5a804e952a9597e552a7ad661cb1523f878
Simas Jakubenas <sjakubenas@gmail.com>
2023-11-14 13:32:43 +0000

    Define game rules function

--- run.py
+++ run.py
@@ -118,7 +118,7 @@ def game_menu_select():
             menu_option(menu_item)
             
         elif menu_item == '4':
-            pass
+            game_rules()
         # Game quit
         elif menu_item == '5':
             menu_item = 'quit'
@@ -189,6 +189,23 @@ def load_game():
     hero_gold = int(last_save[4])
     health_potion = int(last_save[5])
     hero_stats = Hero(hero[0], hero[1], hero[2], hero_gold, health_potion)
+
+def game_rules():
+    """
+    Display game rules
+    """
+    clear()
+    print('Welcome to the world of Tal Rasha traveler\n')
+    print('Navigate the menu by pressing corresponding')
+    print('number or letter and confirm it with "enter" key\n')
+    print('Navigate the map by chosing any of the 4 directions. Once')
+    print('you have reach the edge of map you will not be able to')
+    print('advance further in that direction')
+    print('Kill Duriel to complete the game\n')
+    print('Talk to the Vendor he will be abe to help you to advance')
+    print('to the new area\n')
+    print('Best of luck traveler!\n')
+    print('4. Back to menu')
 
 def hero_selection():
     """
